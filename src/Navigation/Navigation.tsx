147e5e11fbce198555dce47d4d5f87b2ec702d1d
Shashank <nagashashank48@gmail.com>
2024-02-21 14:31:08 +0530

    Responsive Nvigationbar

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -7,10 +7,11 @@ import BentoOutlinedIcon from '@mui/icons-material/BentoOutlined';
 import IDarkmode from '../Interface/IDarkmode';
 import EngineeringOutlinedIcon from '@mui/icons-material/EngineeringOutlined';
 import WorkHistoryOutlinedIcon from '@mui/icons-material/WorkHistoryOutlined';
+import MenuIcon from '@mui/icons-material/Menu';
 const Navigation = (props:IDarkmode) => {
   const [scrollY,setScrollY]=useState<number>(0);
   const [activeScreen,setActiveScreen]=useState<string>("Home");
-  const [showNavbar,setShowNavabar]=useState<boolean>(true)
+  const [showNavbar,setShowNavabar]=useState<boolean>(false)
   const NavbarContainerRef = React.useRef<HTMLDivElement>(null);
 
   useEffect(()=>{
@@ -52,27 +53,27 @@ const Navigation = (props:IDarkmode) => {
       }
   },[scrollY])
 
-  // useEffect(()=>{
-  //   if(scrollY>150 && scrollY<60){
-  //     setShowNavabar(false)
-  //   }
-  //   else{
-  //     setShowNavabar(true)
-  //   }
-  // },[scrollY])
+ 
   return (
-  //  showNavbar?
    <div ref={NavbarContainerRef} className={props.Darkmode?'NavigationbarDarkmode':'NavigationbarLightmode'}>
-      <div className='Navigation'>
+      {window.innerWidth<=600?<div onClick={()=>setShowNavabar(!showNavbar)} className='MenuIcon'><MenuIcon/></div>:<></>}
+      {window.innerWidth>=600?<div className='Navigation'>
       <div className={`${activeScreen==="Contact"&&props.Darkmode?"ActivebuttonDarkmode":activeScreen==="Contact"&&!props.Darkmode?"ActivebuttonLightmode":""} ${props.Darkmode?'TabButtonDarkmode':'TabButtonLightmode'}`} onClick={()=>window.scrollTo({top:3300,behavior:'smooth'})} ><PermContactCalendarOutlinedIcon/>Contact</div>
       <div className={`${activeScreen==="Experience"&&props.Darkmode?"ActivebuttonDarkmode":activeScreen==="Experience"&&!props.Darkmode?"ActivebuttonLightmode":""} ${props.Darkmode?'TabButtonDarkmode':'TabButtonLightmode'}`} onClick={()=>window.scrollTo({top:2500,behavior:'smooth'})}><WorkHistoryOutlinedIcon/>Experience</div>
       <div className={`${activeScreen==="Projects"&&props.Darkmode?"ActivebuttonDarkmode":activeScreen==="Projects"&&!props.Darkmode?"ActivebuttonLightmode":""} ${props.Darkmode?'TabButtonDarkmode':'TabButtonLightmode'}`} onClick={()=>window.scrollTo({top:1900,behavior:'smooth'})}><BentoOutlinedIcon/>Projects</div>
       <div className={`${activeScreen==="Services"&&props.Darkmode?"ActivebuttonDarkmode":activeScreen==="Services"&&!props.Darkmode?"ActivebuttonLightmode":""} ${props.Darkmode?'TabButtonDarkmode':'TabButtonLightmode'}`} onClick={()=>window.scrollTo({top:1250,behavior:'smooth'})}><EngineeringOutlinedIcon/>Services</div>
       <div className={`${activeScreen==="About"&&props.Darkmode?"ActivebuttonDarkmode":activeScreen==="About"&&!props.Darkmode?"ActivebuttonLightmode":""} ${props.Darkmode?'TabButtonDarkmode':'TabButtonLightmode'}`} onClick={()=>window.scrollTo({top:650,behavior:'smooth'})}><Person4OutlinedIcon/>About</div>
       <div className={`${activeScreen==="Home"&&props.Darkmode?"ActivebuttonDarkmode":activeScreen==="Home"&&!props.Darkmode?"ActivebuttonLightmode":""} ${props.Darkmode?'TabButtonDarkmode':'TabButtonLightmode'}`} onClick={()=>window.scrollTo({top:0,behavior:'smooth'})}><HomeOutlinedIcon/>Home</div>
-      </div>
+      </div>:<></>}
+      {window.innerWidth<=600&&showNavbar?<div className='Navigation'>
+      <div className={`${activeScreen==="Home"&&props.Darkmode?"ActivebuttonDarkmode":activeScreen==="Home"&&!props.Darkmode?"ActivebuttonLightmode":""} ${props.Darkmode?'TabButtonDarkmode':'TabButtonLightmode'}`} onClick={()=>window.scrollTo({top:0,behavior:'smooth'})}><HomeOutlinedIcon/>Home</div>
+      <div className={`${activeScreen==="About"&&props.Darkmode?"ActivebuttonDarkmode":activeScreen==="About"&&!props.Darkmode?"ActivebuttonLightmode":""} ${props.Darkmode?'TabButtonDarkmode':'TabButtonLightmode'}`} onClick={()=>window.scrollTo({top:650,behavior:'smooth'})}><Person4OutlinedIcon/>About</div>
+      <div className={`${activeScreen==="Services"&&props.Darkmode?"ActivebuttonDarkmode":activeScreen==="Services"&&!props.Darkmode?"ActivebuttonLightmode":""} ${props.Darkmode?'TabButtonDarkmode':'TabButtonLightmode'}`} onClick={()=>window.scrollTo({top:1250,behavior:'smooth'})}><EngineeringOutlinedIcon/>Services</div>
+      <div className={`${activeScreen==="Projects"&&props.Darkmode?"ActivebuttonDarkmode":activeScreen==="Projects"&&!props.Darkmode?"ActivebuttonLightmode":""} ${props.Darkmode?'TabButtonDarkmode':'TabButtonLightmode'}`} onClick={()=>window.scrollTo({top:1900,behavior:'smooth'})}><BentoOutlinedIcon/>Projects</div>
+      <div className={`${activeScreen==="Experience"&&props.Darkmode?"ActivebuttonDarkmode":activeScreen==="Experience"&&!props.Darkmode?"ActivebuttonLightmode":""} ${props.Darkmode?'TabButtonDarkmode':'TabButtonLightmode'}`} onClick={()=>window.scrollTo({top:2500,behavior:'smooth'})}><WorkHistoryOutlinedIcon/>Experience</div>
+      <div className={`${activeScreen==="Contact"&&props.Darkmode?"ActivebuttonDarkmode":activeScreen==="Contact"&&!props.Darkmode?"ActivebuttonLightmode":""} ${props.Darkmode?'TabButtonDarkmode':'TabButtonLightmode'}`} onClick={()=>window.scrollTo({top:3300,behavior:'smooth'})} ><PermContactCalendarOutlinedIcon/>Contact</div>
+      </div>:<></>}
     </div>
-    // :<></>
   )
 }
 
